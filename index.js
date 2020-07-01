45b1200f7004fde2685e1b468ce50d4362ec7357
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-01 20:44:35 +0530

    Added health check URL

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const server = express()
 
 const PORT = 4001;
 
+server.get('/health', (req, res) => {
+    res.json({status: "Up"})
+})
+
 server.listen(PORT, (err) => {
     if (!err) {
         console.log(`Server started at ${PORT}`)
