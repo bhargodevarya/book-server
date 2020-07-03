4809e910d6fcdede134801506bf2222195834d69
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-03 17:51:09 +0530

    Added service contract to handle book operations

---
+++ service/bookservice.js
@@ -0,0 +1,11 @@
+function saveBook(bookToSave) {
+    console.log("Save book")
+}
+
+function getBook(criteria) {
+    console.log("Get book")
+}
+
+function getAllBooks() {
+    console.log("Get all books")
+}
