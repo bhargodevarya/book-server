c6354b205b4ae9f339c5581ee8f5dd811ed41327
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-03 15:36:44 +0530

    Updating model for book schema

--- model/book.js
+++ model/book.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const schema = new mongoose().schema({})
+const schema = new mongoose().schema({title: 'String', author: 'String', image: 'String', description: 'String', category: [String]})
 
 const book = mongoose.model('Book', schema);
 
