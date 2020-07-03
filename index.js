285ee0b04c59cdf5527d8fed30797882b7ff644f
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-03 15:27:49 +0530

    URI to get all books, intial commit

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ server.get('/health', (req, res) => {
     res.json({status: "Up"})
 })
 
+server.get('/book', (req, res) => {
+    res.json({message: "Will return all books"})
+})
+
 server.listen(PORT, (err) => {
     if (!err) {
         console.log(`Server started at ${PORT}`)
