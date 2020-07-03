21625adf3eb7eae8a81b5ee27a0d520abc6fac76
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-03 18:42:01 +0530

    Added dockerfile and updated util file

--- util/BookUtil.js
+++ util/BookUtil.js
@@ -1,24 +1,37 @@
 //connect to Mongo and create books in Book collection
 //This will be a cmd line utility when I cant access the UI
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Book = require("../model/Book");
 
-const conn = mongoose.connect('mongodb://localhost/book', {useNewUrlParser: true});
+const conn = mongoose.connect("mongodb://localhost/book", {
+  useNewUrlParser: true,
+});
 
-const fiveAmClub = new Book({
-    name: "Savarkar: Echoes from a Forgotten Past",
-    author: "Vikram Sampath",
-    image: "https://book-app-images.s3.amazonaws.com/savarkarBook.jpg",
-    description: "Biography of veer Savarkar",
-    category: ["history", 'biography']
-  })
+// const fiveAmClub = new Book({
+//     name: "Savarkar: Echoes from a Forgotten Past",
+//     author: "Vikram Sampath",
+//     image: "https://book-app-images.s3.amazonaws.com/savarkarBook.jpg",
+//     description: "Biography of veer Savarkar",
+//     category: ["history", 'biography']
+//   })
 
-fiveAmClub.save(function(err, bookSaved){
+// fiveAmClub.save(function(err, bookSaved){
+//     if (err) {
+//         console.log(err)
+//     } else  {
+//         console.log(bookSaved._id)
+//         mongoose.connection.close()
+//     }
+// });
+
+Book.findOne({  "author" : "Vikram Sampath" }, 
+function (err, res) {
     if (err) {
         console.log(err)
-    } else  {
-        console.log(bookSaved._id)
-        mongoose.connection.close()
+    } else {
+        console.log(res)
     }
-});
+    mongoose.connection.close()
+}
+);
