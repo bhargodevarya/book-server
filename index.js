e4c6b5bcd36ea91daf9f93521bd2fa07db6791ea
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-03 18:43:54 +0530

    Reading port from env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const server = express()
 
-const PORT = 4001;
+const PORT = process.env.BOOK_SERVER_PORT | 4001;
 
 server.get('/health', (req, res) => {
     res.json({status: "Up"})
